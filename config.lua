5fdda82eca19f01622f5b360002e02a83adcc073
Ahmaykmewsik <ahmaykmewsik@gmail.com>
2025-12-12 04:03:11 -0700

    build credits ui at runtime not boot

--- config.lua
+++ config.lua
@@ -1,11 +1,13 @@
 SMODS.current_mod.config_tab = assert(SMODS.load_file('config_tab.lua'))
-local mod_description = assert(SMODS.load_file('mod_description.lua'))()
-SMODS.current_mod.custom_ui = function(nodes)
-  nodes[1] = nil
-  nodes[2]['nodes'] = mod_description['nodes']
+local mod_description = assert(SMODS.load_file('mod_description.lua'))
+SMODS.Mods.Qualatro.custom_ui = function(nodes)
+	local desc =  mod_description()
+	nodes[1] = nil
+	nodes[2]['nodes'] = desc.nodes
 end
 return {
   ['music_layers'] = true,
   ['resph_mode'] = false,
   ['resph_textures'] = false,
 }
+
